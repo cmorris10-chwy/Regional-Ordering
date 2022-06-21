b593231ca02077ec1adaf989bcf533912f875608
cmorris10-chwy <cmorris10@chewy.com>
2022-06-21 15:27:14 -0400

    add private label flag and direct import flag to output

--- Automated Process/SQL/create regional ordering recommendations.sql
+++ Automated Process/SQL/create regional ordering recommendations.sql
@@ -59,6 +59,7 @@ create local temp table products on commit preserve rows as
                 ,product_abc_code
                 ,product_discontinued_flag
                 ,product_published_flag
+                ,private_label_flag
                 ,case when ni.item_id is not null then true else false end as is_NEW_ITEM
         from (select distinct product_part_number
                 from inv_base) ib
@@ -435,11 +436,13 @@ select v.vendor_purchaser_code as "Supply Planner"
         ,region
         ,location
         ,p.product_abc_code
+        ,p.private_label_flag
         ,p.is_NEW_ITEM
         ,avg_daily_forecast
         ,supplier
         ,v.vendor_name
         ,v.vendor_distribution_method
+        ,v.direct_import_flag
         ,release_date
         ,ERDD
         ,MOQ
