076340afd58195fbd45a76e354689432845ce3e0
cmorris10-chwy <cmorris10@chewy.com>
2022-06-21 13:26:54 -0400

    create splits tracker file. remove splits from zone file
    this is to clean up and remove the splits % calculation from Tracking zone SQL file and adds it to its own file.

---
+++ KPI Metric Tracking/SQL/Splits Tracking.sql
@@ -0,0 +1,77 @@
+----------------------------------------------------------------------------------
+-- Measure Split% impact for orders w/NB HG items vs. orders w/o NB HG items --
+----------------------------------------------------------------------------------
+-- 7 NB HG/Specialty Planner Codes: ('PPRAKASH','BROSEN','MODZER','BNEUBAUER','MWILSON','SSHARAN','JMALAVIYA')
+drop table if exists regional_orders;
+create local temp table regional_orders
+        (action varchar(10)
+        ,item_id varchar(6)
+        ,region varchar(12)
+        ,location_cd varchar(4)
+        ,supplier varchar(15)
+        ,proposed_qty int
+        ,fc_region_need_percent float)
+on commit preserve rows;
+copy regional_orders
+from local 'C:\Users\cmorris10\Downloads\6-13-22_orders.csv'
+parser fcsvparser(delimiter = ',');
+
+drop table if exists locations;
+create local temp table locations on commit preserve rows as
+        select location_key
+                ,location_code
+        from chewybi.locations l
+        where 1=1
+                and l.fulfillment_active is true
+                and l.location_active_warehouse = 1
+                and l.location_warehouse_type = 0
+                and l.product_company_description = 'Chewy'
+;
+
+drop table if exists splits;
+create local temp table splits on commit preserve rows as
+        select olm.order_key
+                ,min(order_line_released_dttm::date) as order_released_date
+                ,case when count(distinct location_code) > 1 then 1 else 0 end as is_split
+        from chewybi.order_line_measures olm 
+        join chewybi.orders o using(order_key)
+        join locations l on location_key = fulfillment_center_key
+        join chewybi.products p on olm.product_key=p.product_key
+        where order_status not in ('X','J')
+        group by 1
+        having min(order_line_released_dttm::date) between current_date - 90 and current_date - 1
+;
+
+drop table if exists split_prods;
+create local temp table split_prods on commit preserve rows as
+        select s.*
+                ,product_part_number
+        from splits s
+        join chewybi.order_line_measures olm using(order_key)
+        join chewybi.products p using(product_key)
+;
+
+drop table if exists nb_hg_items;
+create local temp table nb_hg_items on commit preserve rows as
+        select distinct item_id
+        from regional_orders
+        order by 1
+;
+
+with a as (
+        select order_released_date
+                ,order_key
+                ,max(is_split) as is_split
+                ,least(1,sum(case when item_id is not null then 1 else 0 end)) as contains_nbhg_item 
+        from split_prods
+        left join nb_hg_items nb on nb.item_id = product_part_number
+        group by 1,2
+)
+select order_released_date
+        ,contains_nbhg_item
+        ,sum(is_split) / count(*) as split_rate
+        ,sum(is_split) as split_order_count
+        ,count(*) as count_orders 
+from a 
+group by 1,2
+order by 1,2;
